fa105d2521ce2e64f0075dfd7b7b15df726334fa
Lucas Barbosa <lucaslukibs@gmail.com>
2025-11-24 22:06:30 -0300

    adjusting variables in the RDS path

--- platform/eks.lb-controller.tf
+++ platform/eks.lb-controller.tf
@@ -1,5 +1,5 @@
 resource "helm_release" "load_balanced_controller" {
-  name       = "aws-load-balancer-controller"   # nome válido
+  name       = "aws-load-balancer-controller"   #
   repository = "https://aws.github.io/eks-charts"
   chart      = "aws-load-balancer-controller"
   version    = "1.13.0"
